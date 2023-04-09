0a65f5ae2b1656c615d5f5d6c06b16335e768424
drelocatelli <andressalocatelli@hotmail.com>
2023-04-09 11:36:58 -0300

    first commit

---
+++ src/scripts/main.js
@@ -0,0 +1,21 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+class App {
+    constructor() {
+        app.whenReady().then(() => {
+            const win = this.init();
+            win.maximize();
+        });
+    }
+    init() {
+        const win = new BrowserWindow({
+            webPreferences: {
+                nodeIntegration: true,
+            },
+        });
+        win.loadFile('./src/static/index.html');
+        return win;
+    }
+}
+new App();
+//# sourceMappingURL=main.js.map
